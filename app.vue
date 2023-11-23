a71602b1142f22d7c613caf6d1fb8bcc0443170d
Pattadon Baongern <pattadonb8@hotmail.com>
2023-11-23 23:57:51 +0700

    🚧 Added google-fonts

--- app.vue
+++ app.vue
@@ -3,3 +3,9 @@
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss" scoped>
+* {
+  font-family: 'Noto Sans Thai', 'Noto Sans', Roboto;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxtjs/google-fonts'],
   content: {
     // ... options
+  },
+  googleFonts: {
+    families: {
+      Roboto: true,
+      'Noto Sans': true,
+      'Noto Sans Thai': true,
+    },
   },
   devtools: {
     enabled: true,
